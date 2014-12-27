3f4fe349fd2346640b103d1d93d384451131a033
kumok <bhh@byu.edu>
2014-12-26 18:58:08 -0700

    Initial Commit

---
+++ pre-course-git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pre-course-git
+//
+//  Created by Duc Ho on 12/26/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
